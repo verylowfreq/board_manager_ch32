fc1400558f90e25e27124e963b257b6ae575ce31
verylowfreq <60875431+verylowfreq@users.noreply.github.com>
2024-11-18 16:56:14 +0900

    Update tool

--- tools/make_board_manager_file.py
+++ tools/make_board_manager_file.py
@@ -10,9 +10,9 @@ UPSTREAM_URL = "https://raw.githubusercontent.com/openwch/board_manager_files/ma
 
 
 MY_VERSION = "1.0.4"
-MY_VERSION_APPENDIX = "+sz6"
+MY_VERSION_APPENDIX = "+sz7"
 MY_VERSION_FULL = f"{MY_VERSION}{MY_VERSION_APPENDIX}"
-MY_ARCHIVE_URL = "https://github.com/verylowfreq/arduino_core_ch32_sz/releases/download/1.0.4+sz6/arduino_core_ch32_sz-1.0.4+sz6.zip"
+MY_ARCHIVE_URL = f"https://github.com/verylowfreq/arduino_core_ch32_sz/releases/download/{MY_VERSION_FULL}/arduino_core_ch32_sz-{MY_VERSION_FULL}.zip"
 MY_ARCHIVE_FILENAME = f"arduino_core_ch32-sz-{MY_VERSION}{MY_VERSION_APPENDIX}.zip"
 
 
@@ -154,6 +154,17 @@ def process_core(src:Any) -> Any:
     print(f'Core definition updated.')
     return src
 
+def replace_beforeinstall(src:Any) -> Any:
+    tools = src["packages"][0]["tools"]
+    beforeinstall_systems = [ tool for tool in tools if tool["name"] == "beforeinstall" ][0]["systems"]
+    beforeinstall_windows = [ host for host in beforeinstall_systems if host["host"] == "i686-mingw32"][0]
+    beforeinstall_windows["url"] = "https://raw.githubusercontent.com/verylowfreq/board_manager_ch32/dev/empty.zip"
+    beforeinstall_windows["archiveFileName"] = "empty.zip"
+    beforeinstall_windows["checksum"] = "SHA-256:0F8F99A6B5F8F197E9BA5F5150BE28F87D23797EB13328FE8035A81D4E826F33"
+    beforeinstall_windows["size"] = "164"
+
+    return src
+
 
 def main() -> None:
 
@@ -182,9 +193,9 @@ def main() -> None:
 
     upstream_defs['packages'][0]['tools'].append(tools)
 
-    # print(json.dumps(upstream_defs, indent=4, separators=(',', ': ')))
+    mydefs = replace_beforeinstall(upstream_defs)
 
-    mydefs = upstream_defs
+    # print(json.dumps(upstream_defs, indent=4, separators=(',', ': ')))
 
     with open(my_json_file, "w") as f:
         json.dump(mydefs, f, indent=4, separators=(',', ': '))
