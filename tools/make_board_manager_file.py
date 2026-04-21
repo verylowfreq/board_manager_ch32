210f65486bc8e0a80d2a1ad27f71729e734e5bc6
Mitsumine Suzu <60875431+verylowfreq@users.noreply.github.com>
2026-04-21 19:12:26 +0900

    Update v2.1.4

--- tools/make_board_manager_file.py
+++ tools/make_board_manager_file.py
@@ -9,7 +9,7 @@ import os
 UPSTREAM_URL = "https://raw.githubusercontent.com/openwch/board_manager_files/main/package_ch32v_index.json"
 
 
-MY_VERSION = "2.1.3"
+MY_VERSION = "2.1.4"
 MY_VERSION_FULL = f"{MY_VERSION}"
 MY_ARCHIVE_URL = f"https://github.com/verylowfreq/arduino_core_ch32_sz/releases/download/{MY_VERSION_FULL}/arduino_core_ch32_sz-{MY_VERSION_FULL}.zip"
 MY_ARCHIVE_FILENAME = f"arduino_core_ch32-sz-{MY_VERSION}.zip"
